47a41672a21aa2968a8d5ab1300fef9b86933125
Gal <gal.aviel97@icloud.com>
2020-06-22 21:30:37 +0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'notepad';
+
+  colors = ['red','green','blue'];
+  fonts = ['arial','consolas','tahoma'];
+  sizes = ['12px','14px','30px'];
+
+  selectedColor = this.colors[0];
+  selectedFont = this.fonts[0];
+  selectedSize = this.sizes[0];
+
+  setColor(value:string){
+    this.selectedColor = value;
+  }
+
+  setFont(value:string){
+    this.selectedFont = value;
+  }
+
+  setSize(value:string){
+    this.selectedSize = value;
+  }
 }
